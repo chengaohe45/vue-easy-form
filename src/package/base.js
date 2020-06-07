f5fad50fe155ca8fe7c646bbd80034f9b261523a
chengaohe45 <chengaohe45@126.com>
2020-06-07 18:33:06 +0800

    修改base.js

--- src/package/base.js
+++ src/package/base.js
@@ -332,9 +332,237 @@ export default {
   },
 
   methods: {
+    /**
+     * 渲染函数
+     * @param {*} createElement 创建VNode的函数
+     * @param {*} config        组件设置
+     * @param {*} isSlotCom     是否插糟的组件，因为事件不一样，插糟数据也不一样
+     * @returns VNode
+     */
+    renderUi(createElement, config, isSlotCom) {
+      var vnode;
+      // 根据vue源代码, VNode是不会被劫持的
+      if (utils.isVNode(config)) {
+        vnode = config;
+      } else if (utils.isObj(config)) {
+        if (!config.name) {
+          console.error("错误的config: ", config);
+          throw "es-base config.name必须存在";
+        }
+
+        // 防止props不存在
+        var configProps = config.props ? config.props : {};
+
+        // 计算出props, attrs
+        var newProps = {};
+        var newAttrs = {};
+        var domProps = {};
+
+        var directives = config.directives
+          ? utils.deepCopy(config.directives)
+          : []; // false, 不是数组也没有事
+
+        var componentName = config.name.toLowerCase
+          ? config.name.toLowerCase()
+          : config.name;
+        if (
+          componentName === constant.TAG_INPUT && // 不区分大小写
+          (configProps.type === constant.TYPE_RADIO ||
+            configProps.type === constant.TYPE_CHECKBOX)
+        ) {
+          if (configProps.type === constant.TYPE_RADIO) {
+            Object.assign(newAttrs, configProps);
+            if (config.hasOwnProperty("value")) {
+              // 用户设置了value的情况
+              newAttrs.checked = config.value === configProps.value;
+            } else {
+              // 没有用户value
+              newAttrs.checked =
+                configProps.hasOwnProperty("checked") &&
+                configProps.checked !== false
+                  ? true
+                  : false;
+            }
+
+            Object.assign(newProps, configProps);
+            if (newProps.hasOwnProperty("checked")) {
+              delete newProps.checked;
+            }
+
+            domProps.checked = newAttrs.checked;
+          } else {
+            var checked = false;
+            if (config.hasOwnProperty("value")) {
+              if (!utils.isUndef(configProps.trueValue)) {
+                // 经测试，若指定了trueValue，无论falseValue是否指定，只有值等于trueValue，checked才为true
+                if (config.value === configProps.trueValue) {
+                  checked = true;
+                } else {
+                  checked = false;
+                }
+              } else if (!utils.isUndef(configProps.falseValue)) {
+                // 经测试：当trueValue没有指定，falseValue指定，只有值等于falseValue，checked才为false
+                if (config.value === configProps.falseValue) {
+                  checked = false;
+                } else {
+                  checked = true;
+                }
+              } else {
+                // 经测试：当trueValue和falseValue没有指定，checked才为!!config.value
+                checked = !!config.value;
+              }
+            } else {
+              checked =
+                configProps.hasOwnProperty("checked") &&
+                configProps.checked !== false
+                  ? true
+                  : false;
+            }
+            Object.assign(newAttrs, configProps);
+            newAttrs.checked = checked;
+
+            Object.assign(newProps, configProps);
+            if (newProps.hasOwnProperty("checked")) {
+              delete newProps.checked;
+            }
+
+            domProps.checked = newAttrs.checked;
+          }
+        } else {
+          var newValue;
+          if (config.hasOwnProperty("value")) {
+            newValue = utils.isRefVal(config.value)
+              ? utils.deepCopy(config.value)
+              : config.value; // 这样防止引用地址被组件内部修改
+          } else {
+            newValue = configProps.value;
+          }
+          if (!constant.FORM_INPUTS.includes(componentName)) {
+            Object.assign(newAttrs, configProps);
+            if (newAttrs.hasOwnProperty("value")) {
+              delete newAttrs.value;
+            }
+            Object.assign(newProps, configProps);
+            newProps.value = newValue;
+          } else {
+            Object.assign(newAttrs, configProps);
+            if (newAttrs.hasOwnProperty("value")) {
+              delete newAttrs.value;
+            }
+
+            Object.assign(newProps, configProps);
+            if (newProps.hasOwnProperty("value")) {
+              delete newProps.value;
+            }
+
+            // 经测试（value）：
+            // textarea必须要在domProps才能显示；
+            // input第一次可以在newAttrs写，之后也要在domProps才能显示值，所以也要是domProps才保险
+            domProps.value = newValue;
+          }
+        }
+
+        vnode = createElement(
+          config.name, // tag name 标签名称 https://www.cnblogs.com/tugenhua0707/p/7528621.html
+          {
+            attrs: newAttrs, //attrs为原生属性
+
+            // 类型要求见：https://cn.vuejs.org/v2/guide/class-and-style.html
+            // config.class必须是String,Object, Array才能有效；当然传入其它类型也可以，只是没有效果，也不会报错，因为createElement会做处理
+            class: config.class,
+            style: config.style, // config.style必须是一个对象才能有效；原理同上
+
+            domProps: domProps, // DOM属性
+
+            props: newProps, // 组件props
+            // 事件监听基于 "on"
+            // 所以不再支持如 "v-on:keyup.enter" 修饰语
+            // 需要手动匹配 KeyCode
+            on: this.$data.emitOn,
+
+            // 仅对于组件，用于监听原生事件，而不是组件内部使用 `vm.$emit` 触发的事件。
+            nativeOn: this.$data.nativeOn,
+
+            // 自定义指令。注意事项：不能对绑定的旧值设值
+            // Vue 会为您持续追踪
+            directives: directives,
+            // Scoped slots in the form of
+            scopedSlots: this.$data.scopedSlots,
+            ref: "__comTarget__"
+          },
+          config.text
+        );
+
+        // 去除多余的原生属性；去不去掉感觉都没有什么，好像没有影响到功能，只是页面上会显示原生属性
+        var componentOptions = vnode.componentOptions;
+        var dataAttrs = {};
+        var comProps =
+          componentOptions && componentOptions.Ctor.options.props
+            ? componentOptions.Ctor.options.props
+            : false;
+        if (
+          comProps &&
+          Object.keys(comProps).length &&
+          Object.keys(configProps).length
+        ) {
+          var comPropsKeys = Object.keys(comProps); // 经测试：就算在定义中声明为中划线形式，这里也会返回驼峰式，如 'text-str' => 'textStr'
+          for (var key in configProps) {
+            if (!comPropsKeys.includes(key)) {
+              dataAttrs[key] = configProps[key];
+            }
+          }
+          if (vnode.data) {
+            vnode.data.attrs = dataAttrs;
+          }
+        }
+
+        newAttrs = null;
+        newProps = null;
+        domProps = null;
+        directives = null;
+
+        configProps = null;
+      } else {
+        var options = {
+          global: dataCache.getGlobal(this.config.__formId), // 直接取组件__formId，插糟是没有__formId的
+          rootData: dataCache.getRoot(this.config.__formId), // 兼容1.7.0以前，不包括1.7.0
+          root: dataCache.getRoot(this.config.__formId),
+          idxChain: this.info.idxChain,
+          index: this.info.index,
+          pathKey: this.info.pathKey,
+          $hidden: dataCache.getHiddenFunc(this.config.__formId)
+        };
+
+        vnode = config(options);
+        if (!utils.isVNode(vnode)) {
+          if (
+            utils.isStr(vnode) ||
+            utils.isUndef(vnode) ||
+            utils.isNum(vnode) ||
+            utils.isBool(vnode) ||
+            utils.isNull(vnode)
+          ) {
+            vnode = createElement("span", vnode); // 经验证，这样写没有问题：vnode不用加""
+          } else {
+            console.error("错误的config: ", config);
+            throw "es-base config当是一个函数时，其返回值必须是一个虚拟节点或字符串";
+          }
+        }
+      }
+
+      // 这个不能删除：vue机制，主要是为了执行this.config.__refreshIndex；当表单改变时，同步更新页面
+      if (this.config.__refreshIndex && this.__slotUpdateTime) {
+        // 永远都不会进入这，因为__slotUpdateTime没有值的
+        this.__slotUpdateTime++;
+      }
+
+      return vnode;
+    },
+
     initUi() {
-      this.createOn();
-      this.createScopedSlots();
+      this.$data.emitOn = this.createEmitOn(this.config);
+      this.$data.nativeOn = this.createNativeOn(this.config);
+      this.$data.scopedSlots = this.createScopedSlots(this.config);
     },
 
     eventHandler(eventName, args) {
@@ -429,15 +657,15 @@ export default {
     },
 
     /**
-     * 创建所需要监听的事件
+     * 创建emit派发所需要监听的事件
      */
-    createOn() {
+    createEmitOn(config) {
       var _thisVm = this;
 
-      var hasOwnValue = this.config.hasOwnProperty("value");
+      var hasOwnValue = config.hasOwnProperty("value");
       var emitEvents;
-      if (this.config.__emitEvents) {
-        emitEvents = utils.deepCopy(this.config.__emitEvents);
+      if (config.__emitEvents) {
+        emitEvents = utils.deepCopy(config.__emitEvents);
         if (hasOwnValue && !emitEvents.includes(constant.INPUT_EVENT)) {
           emitEvents.push(constant.INPUT_EVENT);
         }
@@ -467,13 +695,23 @@ export default {
         }
       });
 
-      this.$data.emitOn = Object.keys(emitOn).length > 0 ? emitOn : null;
+      if (Object.keys(emitOn).length > 0) {
+        return emitOn;
+      } else {
+        _thisVm = undefined; // 没有关联清除
+        return null;
+      }
+    },
 
-      //原生事件
+    /**
+     * 创建原生事件所需要监听的事件
+     */
+    createNativeOn(config) {
       // emit发出的事件
-      if (this.config.__nativeEvents && this.config.__nativeEvents.length > 0) {
+      if (config.__nativeEvents && config.__nativeEvents.length > 0) {
+        var _thisVm = this;
         var nativeOn = {};
-        var nativeEvents = utils.deepCopy(this.config.__nativeEvents);
+        var nativeEvents = utils.deepCopy(config.__nativeEvents);
         nativeEvents.forEach(eventName => {
           nativeOn[eventName] = function() {
             _thisVm.eventHandler(
@@ -483,27 +721,27 @@ export default {
           };
         });
 
-        this.$data.nativeOn =
-          Object.keys(nativeOn).length > 0 ? nativeOn : null;
+        if (Object.keys(nativeOn).length > 0) {
+          return nativeOn;
+        } else {
+          _thisVm = undefined;
+          return null;
+        }
       } else {
-        this.$data.nativeOn = null;
-      }
-
-      if (!this.$data.emitOn && !this.$data.nativeOn) {
-        _thisVm = undefined; // 没有关联清除
+        return null;
       }
     },
 
-    createScopedSlots() {
-      var scopedSlots = this.config.scopedSlots;
+    createScopedSlots(config) {
+      var scopedSlots = config.scopedSlots;
       if (scopedSlots) {
         var newScopedSlots = {};
         for (var key in scopedSlots) {
           newScopedSlots[key] = this.newSlotFunc(scopedSlots[key]);
         }
-        this.$data.scopedSlots = newScopedSlots;
+        return newScopedSlots;
       } else {
-        this.$data.scopedSlots = undefined;
+        return undefined;
       }
     },
 
